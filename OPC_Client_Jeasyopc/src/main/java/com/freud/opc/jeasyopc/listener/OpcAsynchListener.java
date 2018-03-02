7428a8d4467472e6cb0cfc51c3a315061cc9c496
杨鹏 <peng3.yang@dhc.com.cn>
2018-03-02 09:37:51 +0800

    添加依赖

--- OPC_Client_Jeasyopc/src/main/java/com/freud/opc/jeasyopc/listener/OpcAsynchListener.java
+++ OPC_Client_Jeasyopc/src/main/java/com/freud/opc/jeasyopc/listener/OpcAsynchListener.java
@@ -1,4 +1,4 @@
-package com.freud.opc.jeasyopc.listener;
+package main.java.com.freud.opc.jeasyopc.listener;
 
 import javafish.clients.opc.JCustomOpc;
 import javafish.clients.opc.asynch.AsynchEvent;
